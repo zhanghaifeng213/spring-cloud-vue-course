76d2db87bf023e30f22d6ea0d2ed691a93c56f74
张海峰 <zhanghf_123@sina.com>
2023-07-04 16:13:38 +0800

    1. 小节表section表结构设计 2. 生成section表持久层代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -15,3 +15,19 @@ create table `test`(
 ) engine=innodb default charset=utf8mb4 comment='测试';
 insert into `test` (id, name) values (1, '测试');
 insert into `test` (id, name) values (2, '测试2');
+
+drop table if exists `section`;
+create table `section` (
+	`id` char(8) not null default '' comment 'ID',
+    `title` varchar(50) not null comment '标题',
+    `course_id` char(8) comment '课程｜course.id',
+    `chapter_id` char(8) comment '大章｜chapter.id',
+    `video` varchar(200) comment '视频',
+    `time` INT COMMENT '时长｜单位秒',
+    `charge` char(1) comment '收费｜C 收费；F 免费',
+    `sort` int comment '顺序',
+    `created_at` DATETIME(3) comment '创建时间',
+    `update_at` DATETIME(3) comment '修改时间',
+    primary key(`id`)
+) engine=innodb default charset=utf8mb4 comment='小节';
+insert into `section` (id, title, course_id, chapter_id, video, time, charge, sort, created_at, update_at) values ('00000001', '测试小节01', '00000001', '00000000', '', 500, 'F', 1, now(), now());
