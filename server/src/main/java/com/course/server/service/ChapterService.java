03c46a940617a1117f835402a7f58d65f9b41276
张海峰 <zhanghf_123@sina.com>
2023-07-04 11:08:59 +0800

    1. 分页参数，axios的post请求默认是以流的方式传递参数，所以controller里的参数要加@RequestBody注解

--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -3,8 +3,10 @@ package com.course.server.service;
 import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
 import com.course.server.dto.ChapterDto;
+import com.course.server.dto.PageDto;
 import com.course.server.mapper.ChapterMapper;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +19,13 @@ public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
 
-    public List<ChapterDto> list() {
-        PageHelper.startPage(1, 1);
+    public void list(PageDto pageDto) {
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
         ChapterExample chapterExample = new ChapterExample();
         List<Chapter> chapters = chapterMapper.selectByExample(chapterExample);
+        PageInfo<Chapter> pageInfo = new PageInfo<>(chapters);
+        pageDto.setTotal(pageInfo.getTotal());
+
         List<ChapterDto> chapterDtos = new ArrayList<ChapterDto>();
         for (int i = 0; i < chapters.size(); i++) {
             Chapter chapter = chapters.get(i);
@@ -28,6 +33,6 @@ public class ChapterService {
             BeanUtils.copyProperties(chapter, chapterDto);
             chapterDtos.add(chapterDto);
         }
-        return chapterDtos;
+        pageDto.setList(chapterDtos);
     }
 }
