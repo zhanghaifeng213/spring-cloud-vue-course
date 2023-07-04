aca21f0e7d0b9c62934bd8b3ca1de36e3f966b70
张海峰 <zhanghf_123@sina.com>
2023-07-04 11:43:55 +0800

    1. 增加新增大章功能，后端代码开发，完成前后端联调，保存成功

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -26,4 +26,11 @@ public class ChapterController {
         chapterService.list(pageDto);
         return pageDto;
     }
+
+    @RequestMapping("/save")
+    public ChapterDto save(@RequestBody ChapterDto chapterDto) {
+        LOG.info("chapterDto: {}", chapterDto);
+        chapterService.save(chapterDto);
+        return chapterDto;
+    }
 }
