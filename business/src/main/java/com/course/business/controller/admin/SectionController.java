da11c89a3f9ad8b20ba0a4a8c106d953ffaf72e9
张海峰 <zhanghf_123@sina.com>
2023-07-06 09:14:55 +0800

    1. 前后端模版，增加字段校验

--- business/src/main/java/com/course/business/controller/admin/SectionController.java
+++ business/src/main/java/com/course/business/controller/admin/SectionController.java
@@ -35,7 +35,10 @@ public class SectionController {
     public ResponseDto save(@RequestBody SectionDto sectionDto) {
         LOG.info("sectionDto: {}", sectionDto);
         // 保存校验
-
+        ValidatorUtil.require(sectionDto.getId(), "ID");
+        ValidatorUtil.require(sectionDto.getTitle(), "标题");
+        ValidatorUtil.length(sectionDto.getTitle(), "标题", 1, 50);
+        ValidatorUtil.length(sectionDto.getVideo(), "视频", 1, 200);
         ResponseDto responseDto = new ResponseDto();
         sectionService.save(sectionDto);
         responseDto.setContent(sectionDto);
