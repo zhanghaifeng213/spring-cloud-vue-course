52e540ec102409b30588d3703393525bd0e9fd71
张海峰 <zhanghf_123@sina.com>
2023-07-08 10:34:28 +0800

    1. 通过生成器，完成基本的课程管理功能

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -31,3 +31,23 @@ create table `section` (
     primary key(`id`)
 ) engine=innodb default charset=utf8mb4 comment='小节';
 insert into `section` (id, title, course_id, chapter_id, video, time, charge, sort, created_at, update_at) values ('00000001', '测试小节01', '00000001', '00000000', '', 500, 'F', 1, now(), now());
+
+drop table if exists course;
+create table course (
+	id char(8) not null default '' comment 'id',
+    name varchar(50) not null comment '名称',
+    summary varchar(2000) comment '概述',
+    time int default 0 comment '时长｜单位秒',
+    price decimal(8, 2) not null default 0.00 comment '价格（元）',
+    image varchar(100) comment '封面',
+    level char(1) not null comment '级别｜ONE("1", "初级"), TWO("2", "中级"), THREE("3", "高级")',
+    charge char(1) comment '收费｜CHARGE("C", "收费"), FREE("F", "免费")',
+    status char(1) comment '状态｜PUBLISH("P", "发布"), DRAFT("D", "草稿")',
+    enroll integer default 0 comment '报名数',
+    sort int comment '顺序',
+    created_at datetime(3) comment '创建时间',
+    updated_at datetime(3) comment '修改时间',
+    primary key (id)
+) engine=innodb default charset=utf8mb4 comment='课程';
+INSERT INTO course (id, name, summary, time, price, image, level, charge, status, enroll, sort, created_at, updated_at)
+VALUES ('00000001', '测试课程01', '这是一门测试课程', 7200, 19.9, '', 0, 'C', 'D', 100, 0, now(), now());
---
+++ server/src/main/java/com/course/server/service/CourseService.java
@@ -0,0 +1,64 @@
+package com.course.server.service;
+
+import com.course.server.domain.Course;
+import com.course.server.domain.CourseExample;
+import com.course.server.dto.CourseDto;
+import com.course.server.dto.PageDto;
+import com.course.server.mapper.CourseMapper;
+import com.course.server.util.CopyUtil;
+import com.course.server.util.UuidUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Date;
+
+@Service
+public class CourseService {
+    @Resource
+    private CourseMapper courseMapper;
+
+    public void list(PageDto pageDto) {
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
+        CourseExample courseExample = new CourseExample();
+        courseExample.setOrderByClause("sort asc");
+        List<Course> courses = courseMapper.selectByExample(courseExample);
+        PageInfo<Course> pageInfo = new PageInfo<>(courses);
+        pageDto.setTotal(pageInfo.getTotal());
+
+        List<CourseDto> courseDtos = new ArrayList<CourseDto>();
+        for (int i = 0; i < courses.size(); i++) {
+            Course course = courses.get(i);
+            CourseDto courseDto = new CourseDto();
+            BeanUtils.copyProperties(course, courseDto);
+            courseDtos.add(courseDto);
+        }
+        pageDto.setList(courseDtos);
+    }
+    public void save(CourseDto courseDto) {
+        Course course = CopyUtil.copy(courseDto, Course.class);
+        if(StringUtils.isEmpty(courseDto.getId())) {
+            this.insert(course);
+        } else {
+            this.update(course);
+        }
+    }
+    private void insert(Course course) {
+        Date now = new Date();
+        course.setId(UuidUtil.getShortUuid());
+        courseMapper.insert(course);
+    }
+
+    private void update(Course course) {
+        courseMapper.updateByPrimaryKey(course);
+    }
+
+    public void delete(String id) {
+        courseMapper.deleteByPrimaryKey(id);
+    }
+}
