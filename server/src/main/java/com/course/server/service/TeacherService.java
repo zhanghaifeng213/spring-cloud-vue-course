834c949f417fb4b68d538cb5419825c65e3c6041
张海峰 <zhanghf_123@sina.com>
2023-07-09 16:05:05 +0800

    1. 讲师管理

---
+++ server/src/main/java/com/course/server/service/TeacherService.java
@@ -0,0 +1,61 @@
+package com.course.server.service;
+
+import com.course.server.domain.Teacher;
+import com.course.server.domain.TeacherExample;
+import com.course.server.dto.TeacherDto;
+import com.course.server.dto.PageDto;
+import com.course.server.mapper.TeacherMapper;
+import com.course.server.util.CopyUtil;
+import com.course.server.util.UuidUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TeacherService {
+    @Resource
+    private TeacherMapper teacherMapper;
+
+    public void list(PageDto pageDto) {
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
+        TeacherExample teacherExample = new TeacherExample();
+        List<Teacher> teachers = teacherMapper.selectByExample(teacherExample);
+        PageInfo<Teacher> pageInfo = new PageInfo<>(teachers);
+        pageDto.setTotal(pageInfo.getTotal());
+
+        List<TeacherDto> teacherDtos = new ArrayList<TeacherDto>();
+        for (int i = 0; i < teachers.size(); i++) {
+            Teacher teacher = teachers.get(i);
+            TeacherDto teacherDto = new TeacherDto();
+            BeanUtils.copyProperties(teacher, teacherDto);
+            teacherDtos.add(teacherDto);
+        }
+        pageDto.setList(teacherDtos);
+    }
+    public void save(TeacherDto teacherDto) {
+        Teacher teacher = CopyUtil.copy(teacherDto, Teacher.class);
+        if(StringUtils.isEmpty(teacherDto.getId())) {
+            this.insert(teacher);
+        } else {
+            this.update(teacher);
+        }
+    }
+    private void insert(Teacher teacher) {
+        teacher.setId(UuidUtil.getShortUuid());
+        teacherMapper.insert(teacher);
+    }
+
+    private void update(Teacher teacher) {
+        teacherMapper.updateByPrimaryKey(teacher);
+    }
+
+    public void delete(String id) {
+        teacherMapper.deleteByPrimaryKey(id);
+    }
+}
