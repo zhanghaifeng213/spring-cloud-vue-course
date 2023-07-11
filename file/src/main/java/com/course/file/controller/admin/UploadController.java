4e5cf56387bcbd27c55816fd5cbe534e1feca05d
张海峰 <zhanghf_123@sina.com>
2023-07-11 21:34:28 +0800

    1. 文件上传功能开发：上传文件时保存文件记录

--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -1,7 +1,9 @@
 package com.course.file.controller.admin;
 
 import com.course.server.domain.Test;
+import com.course.server.dto.FileDto;
 import com.course.server.dto.ResponseDto;
+import com.course.server.service.FileService;
 import com.course.server.service.TestService;
 import com.course.server.util.UuidUtil;
 import org.slf4j.Logger;
@@ -27,6 +29,9 @@ public class UploadController {
     @Value("${file.domain}")
     private String FILE_DOMAIN;
 
+    @Resource
+    private FileService fileService;
+
     @Value("${file.path}")
     private String FILE_PATH;
     @RequestMapping("/upload")
@@ -36,14 +41,26 @@ public class UploadController {
         LOG.info(String.valueOf(file.getSize()));
 
         // 保存文件到本地
-        String fileName = file.getOriginalFilename();
         String key = UuidUtil.getShortUuid();
-        String fullPath = FILE_PATH + "/teacher/" + key + "-" + fileName;
+        String fileName = file.getOriginalFilename();
+        String suffix = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
+        String path = "teacher/" + key + "." + suffix;
+        String fullPath = FILE_PATH + path;
         File dest = new File(fullPath);
         file.transferTo(dest);
         LOG.info(dest.getAbsolutePath());
+
+        LOG.info("保存文件记录开始");
+        FileDto fileDto = new FileDto();
+        fileDto.setPath(path);
+        fileDto.setName(fileName);
+        fileDto.setSize(Math.toIntExact(file.getSize()));
+        fileDto.setSuffix(suffix);
+        fileDto.setUse("");
+        fileService.save(fileDto);
+
         ResponseDto<Object> objectResponseDto = new ResponseDto();
-        objectResponseDto.setContent(FILE_DOMAIN + "/f/teacher/" + key + "-" + fileName);
+        objectResponseDto.setContent(FILE_DOMAIN + path);
         return objectResponseDto;
     }
 }
