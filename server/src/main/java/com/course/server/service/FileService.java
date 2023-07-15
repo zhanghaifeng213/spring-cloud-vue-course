0b089dc6c4018402956c0d4e9aacb3ec1026ee8b
张海峰 <zhanghf_123@sina.com>
2023-07-15 12:53:38 +0800

    9-2. 大文件断点续传与极速秒传：根据文件标识来新增或修改文件记录，修改分片索引从1开始

--- server/src/main/java/com/course/server/service/FileService.java
+++ server/src/main/java/com/course/server/service/FileService.java
@@ -11,6 +11,7 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
@@ -41,10 +42,12 @@ public class FileService {
     }
     public void save(FileDto fileDto) {
         File file = CopyUtil.copy(fileDto, File.class);
-        if(StringUtils.isEmpty(fileDto.getId())) {
+        File fileDb = selectByKey(fileDto.getKey());
+        if(fileDb == null) {
             this.insert(file);
         } else {
-            this.update(file);
+            fileDb.setShardIndex(fileDto.getShardIndex());
+            this.update(fileDb);
         }
     }
     private void insert(File file) {
@@ -59,5 +62,16 @@ public class FileService {
 
     public void delete(String id) {
         fileMapper.deleteByPrimaryKey(id);
+    }
+
+    public File selectByKey(String key) {
+        FileExample example = new FileExample();
+        example.createCriteria().andKeyEqualTo(key);
+        List<File> files = fileMapper.selectByExample(example);
+        if(CollectionUtils.isEmpty(files)) {
+            return null;
+        } else {
+            return files.get(0);
+        }
     }
 }
