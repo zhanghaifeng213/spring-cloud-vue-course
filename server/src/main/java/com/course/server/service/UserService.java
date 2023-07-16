ba3c56f363f81059c8f666e0bc30d4760a665188
张海峰 <zhanghf_123@sina.com>
2023-07-16 14:13:48 +0800

    11-1 1. 用户管理与登录：增加用户名是否已存在校验 2. 增加自定义业务异常

---
+++ server/src/main/java/com/course/server/service/UserService.java
@@ -0,0 +1,84 @@
+package com.course.server.service;
+
+import com.course.server.domain.User;
+import com.course.server.domain.UserExample;
+import com.course.server.dto.UserDto;
+import com.course.server.dto.PageDto;
+import com.course.server.exception.BusinessException;
+import com.course.server.exception.BusinessExceptionCode;
+import com.course.server.mapper.UserMapper;
+import com.course.server.util.CopyUtil;
+import com.course.server.util.UuidUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    @Resource
+    private UserMapper userMapper;
+
+    public void list(PageDto pageDto) {
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
+        UserExample userExample = new UserExample();
+        List<User> users = userMapper.selectByExample(userExample);
+        PageInfo<User> pageInfo = new PageInfo<>(users);
+        pageDto.setTotal(pageInfo.getTotal());
+
+        List<UserDto> userDtos = new ArrayList<UserDto>();
+        for (int i = 0; i < users.size(); i++) {
+            User user = users.get(i);
+            UserDto userDto = new UserDto();
+            BeanUtils.copyProperties(user, userDto);
+            userDtos.add(userDto);
+        }
+        pageDto.setList(userDtos);
+    }
+    public void save(UserDto userDto) {
+        User user = CopyUtil.copy(userDto, User.class);
+        if(StringUtils.isEmpty(userDto.getId())) {
+            this.insert(user);
+        } else {
+            this.update(user);
+        }
+    }
+    private void insert(User user) {
+        user.setId(UuidUtil.getShortUuid());
+        User userDb = this.selectByLoginName(user.getLoginName());
+        if(userDb != null) {
+            throw  new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+        }
+        userMapper.insert(user);
+    }
+
+    private void update(User user) {
+        userMapper.updateByPrimaryKey(user);
+    }
+
+    public void delete(String id) {
+        userMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 根据登录名查询用户信息
+     * @param loginName
+     * @return
+     */
+    public User selectByLoginName(String loginName) {
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().andLoginNameEqualTo(loginName);
+        List<User> users = userMapper.selectByExample(userExample);
+        if(CollectionUtils.isEmpty(users)) {
+            return null;
+        } else {
+            return users.get(0);
+        }
+    }
+}
