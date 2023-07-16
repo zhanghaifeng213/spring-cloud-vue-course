accfec993e18d5b2abd37b3835405df68b01a6bb
张海峰 <zhanghf_123@sina.com>
2023-07-16 16:12:55 +0800

    11-2 1. 用户管理与登录：密码加密传输和加密存储

--- system/src/main/java/com/course/system/controller/admin/UserController.java
+++ system/src/main/java/com/course/system/controller/admin/UserController.java
@@ -10,6 +10,7 @@ import com.course.server.service.UserService;
 import com.course.server.util.ValidatorUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -35,6 +36,7 @@ public class UserController {
     @PostMapping("/save")
     public ResponseDto save(@RequestBody UserDto userDto) {
         LOG.info("userDto: {}", userDto);
+        userDto.setPassword(DigestUtils.md5DigestAsHex(userDto.getPassword().getBytes()));
         // 保存校验
         ValidatorUtil.require(userDto.getLoginName(), "登录名");
         ValidatorUtil.length(userDto.getLoginName(), "登录名", 1, 50);
